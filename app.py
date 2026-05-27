807cc4eec893bc4ebe6daa5ef7aa92a9b86df08e
Samson JK <samsonjeeva15@gmail.com>
2026-05-28 00:03:00 +0530

    feat: update home route message and add Docker Build & Push GitHub Actions workflow

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Python Docker GitHub Actions Demo"
+    return "Python Docker build & push GitHub Actions Demo"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
